a8e12c226faeeebe4a0e14e3b8f4c82b63f19146
Ruben Garcia <rubenhoodspain@gmail.com>
2023-01-17 18:48:54 +0100

    first commit

---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,32 @@
+import { useState } from "react";
+import { NavLink } from "react-router-dom";
+import { RiCloseLine } from "react-icons/ri";
+
+import { logo } from "../assets";
+import { links } from "../assets/constants";
+
+const NavLinks = ({handleClick}) => (
+  <div className="mt-10">
+    {links.map((item) => (
+      <NavLink key={item.name} to={item.to} className="flex flex-row justify-start items-center my-8
+      text-sm font-medium text-gray-400 hover:text-cyan-400" onClick={() => handleClick && handleClick()}>
+        {item.name}
+      </NavLink>
+    ))}
+  </div>
+);
+
+const Sidebar = () => {
+  const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
+
+  return (
+    <>
+      <div className="md:flex hidden flex-col w-[240px] py-10 px-4 bg-[#191624]">
+        <img src={logo} alt="logo" className="w-full h-14 object-contain" />
+        <NavLinks />
+      </div>
+    </>
+  )
+};
+
+export default Sidebar;
---
+++ src/redux/services/shazamCore.js
@@ -0,0 +1,20 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+
+export const shazamCoreApi = createApi({
+  reducerPath: 'shazamCoreApi',
+  baseQuery: fetchBaseQuery({
+    baseUrl: 'https://shazam-core.p.rapidapi.com/v1',
+    prepareHeaders: (headers) => {
+      headers.set('X-RapidAPI-Key', 'be23909861msh200e5b73bb07edep113e90jsn4b4204317941');
+
+      return headers;
+    },
+  }),
+  endpoints: (builder) => ({
+    getTopCharts: builder.query({ query: () => '/charts/world' }),
+  }),
+});
+
+export const {
+  useGetTopChartsQuery,
+} = shazamCoreApi;
